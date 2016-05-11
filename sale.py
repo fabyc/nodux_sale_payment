8ccd765c8f2333173965d787b968332f0bcc862c
Tatiana Quizhpe <tatianaq@nodux.ec>
2016-05-11 12:45:45 -0500

    Fixed paid and residual amount

--- sale.py
+++ sale.py
@@ -46,40 +46,40 @@ class Card(ModelSQL, ModelView):
     banco =  fields.Many2One('bank', 'Banco', states={
                 'readonly': ~Eval('active', True),
                 })
-    
+
     @classmethod
     def __setup__(cls):
         super(Card, cls).__setup__()
-        
+
     @classmethod
     def search_rec_name(cls, name, clause):
         return ['OR',
             ('banco',) + tuple(clause[1:]),
             (cls._rec_name,) + tuple(clause[1:]),
             ]
-            
+
     def get_rec_name(self, name):
         if self.banco:
             return self.name + ' - ' + self.banco.party.name
         else:
             return self.name
-            
+
 class Sale():
     __name__ = 'sale.sale'
     acumulativo = fields.Boolean ('Plan acumulativo', help = "Seleccione si realizara un plan acumulativo",  states={
                 'readonly': ~Eval('active', True),
                 })
-            
+
     tipo_p = fields.Char('Tipo de Pago')
-    
+
     recibido = fields.Numeric('Valor recibido del cliente',
         digits=(16, Eval('currency_digits', 2)),
         depends=['currency_digits'])
-        
+
     cambio = fields.Numeric('CAMBIO',
         digits=(16, Eval('currency_digits', 2)),
         depends=['currency_digits'])
-        
+
     banco =  fields.Many2One('bank', 'Banco', states={
                 'readonly': ~Eval('active', True),
                 'invisible': Eval('tipo_p') != 'cheque',
@@ -130,7 +130,7 @@ class Sale():
                 'readonly': ~Eval('active', True),
                 'invisible': Eval('tipo_p') != 'deposito',
                 })
-                
+
     @classmethod
     def __setup__(cls):
         super(Sale, cls).__setup__()
@@ -139,7 +139,7 @@ class Sale():
                     'invisible':~Eval('acumulativo', False)
                     },
                 })
-    
+
     @fields.depends('acumulativo', 'party')
     def on_change_acumulativo(self):
         pool = Pool()
@@ -149,13 +149,87 @@ class Sale():
                 res['acumulativo'] = False
                 return res
         return res
-    
+
     def get_amount2words(self, value):
         if conversor:
             return (conversor.cardinal(int(value))).upper()
         else:
             return ''
-                   
+
+    @classmethod
+    def get_paid_amount(cls, sales, names):
+        pool = Pool()
+
+        Invoice = pool.get('account.invoice')
+        MoveLine = pool.get('account.move.line')
+        InvoiceAccountMoveLine = pool.get('account.invoice-account.move.line')
+        amount_unreconciled = Decimal(0.0)
+        amount = Decimal(0.0)
+        id_i = None
+        result = {n: {s.id: Decimal(0) for s in sales} for n in names}
+        invoice = ""
+        for name in names:
+            for sale in sales:
+                amount_unreconciled = Decimal(0.0)
+                amount = Decimal(0.0)
+                invoices = Invoice.search([('description','=', sale.reference), ('description', '!=', None)])
+                if invoices:
+                    for i in invoices:
+                        invoice = i.number
+                move_lines = MoveLine.search([
+                        ('description', '=', invoice),
+                    ])
+                for line in move_lines:
+                    amount += line.credit
+                
+                if sale.payments:
+                    for payment in sale.payments:
+                        amount += payment.amount
+                        
+                if amount:
+                    result[name][sale.id] = amount
+                else:
+                    for payment in sale.payments:
+                        result[name][sale.id] += payment.amount
+        return result
+
+    @classmethod
+    def get_residual_amount(cls, sales, names):
+        pool = Pool()
+        Invoice = pool.get('account.invoice')
+        MoveLine = pool.get('account.move.line')
+        InvoiceAccountMoveLine = pool.get('account.invoice-account.move.line')
+        amount_unreconciled = Decimal(0.0)
+        amount = Decimal(0.0)
+        original = Decimal(0.0)
+        id_i = None
+        invoice = ""
+        result = {n: {s.id: Decimal(0) for s in sales} for n in names}
+        for name in names:
+            for sale in sales:
+                amount_unreconciled = Decimal(0.0)
+                amount = Decimal(0.0)
+                invoices = Invoice.search([('description','=', sale.reference), ('description', '!=', None)])
+                if invoices:
+                    for i in invoices:
+                        invoice = i.number
+                move_lines = MoveLine.search([
+                        ('description', '=', invoice),
+                    ])
+                for line in move_lines:
+                    amount += line.credit
+                if sale.total_amount:
+                    original = sale.total_amount
+                    
+                if sale.payments:
+                    for payment in sale.payments:
+                        amount += payment.amount
+                if amount:
+                    result[name][sale.id] = original - amount
+                else:
+                    result[name][sale.id] = sale.total_amount - sale.paid_amount
+        return result
+
     @classmethod
     @ModelView.button
     def process(cls, sales):
@@ -177,7 +251,7 @@ class Sale():
             cls.proceed(process)
         if done:
             cls.do(done)
-    
+
     @classmethod
     def workflow_to_end(cls, sales):
         pool = Pool()
@@ -217,7 +291,7 @@ class Sale():
                     payment.save()
             if sale.is_done():
                 cls.do([sale])
-                
+
 class SalePaymentForm():
     'Sale Payment Form'
     __name__ = 'sale.payment.form'
@@ -236,7 +310,7 @@ class SalePaymentForm():
     tipo_p = fields.Function(fields.Char('Tipo de Pago'),'on_change_with_journal')
     """
     tipo_p =fields.Selection([
-            ('',''), 
+            ('',''),
             ('efectivo','Efectivo'),
             ('tarjeta','Tarjeta de Credito'),
             ('deposito','Deposito'),
@@ -296,8 +370,11 @@ class SalePaymentForm():
     amount = fields.Numeric('Payment amount', required=True,
         digits=(16, Eval('currency_digits', 2)),
         depends=['currency_digits'])
-        
-     
+
+    @classmethod
+    def __setup__(cls):
+        super(SalePaymentForm, cls).__setup__()
+
     @fields.depends('payment_amount', 'recibido')
     def on_change_recibido(self):
         result = {}
@@ -307,7 +384,7 @@ class SalePaymentForm():
             cambio = (self.recibido) - (self.payment_amount)
         result['cambio_cliente'] = cambio
         return result
-            
+
     @fields.depends('journal', 'party')
     def on_change_journal(self):
         if self.journal:
@@ -315,7 +392,7 @@ class SalePaymentForm():
             pool = Pool()
             Statement=pool.get('account.statement')
             statement = Statement.search([('journal', '=', self.journal.id)])
-            
+
             if statement:
                 for s in statement:
                     result['tipo_p'] = s.tipo_pago
@@ -324,22 +401,22 @@ class SalePaymentForm():
                     pass
                 else:
                     self.raise_user_error('No ha configurado el tipo de pago. \n-Seleccione el estado de cuenta en "Todos los estados de cuenta" \n-Seleccione forma de pago.')
-            else: 
+            else:
                  self.raise_user_error('No ha creado el estado de cuenta para el punto de venta')
             if tipo_p == 'cheque':
                 titular = self.party.name
                 result['titular'] = titular
         return result
-            
+
     @staticmethod
     def default_cambio_cliente():
-        return Decimal(0.0)    
-    
+        return Decimal(0.0)
+
 class WizardSalePayment(Wizard):
     'Wizard Sale Payment'
-    __name__ = 'sale.payment'  
+    __name__ = 'sale.payment'
     print_ = StateAction('nodux_sale_payment.report_invoice_pos')
-    
+
     @classmethod
     def __setup__(cls):
         super(WizardSalePayment, cls).__setup__()
@@ -357,13 +434,13 @@ class WizardSalePayment(Wizard):
         sale_device = sale.sale_device or user.sale_device or False
         Date = pool.get('ir.date')
         Statement=pool.get('account.statement')
-        
+
         ModelData = pool.get('ir.model.data')
         User = pool.get('res.user')
         Group = pool.get('res.group')
         origin = str(sale)
         def in_group():
-            
+
             group = Group(ModelData.get_id('nodux_sale_payment',
                     'group_stock_force'))
             transaction = Transaction()
@@ -374,12 +451,12 @@ class WizardSalePayment(Wizard):
                 return True
             user = User(user_id)
             return origin and group in user.groups
-            
+
         if sale_device.journal:
             statement = Statement.search([('journal', '=', sale_device.journal.id), ('state', '=', 'draft')], order=[('date', 'DESC')])
         else:
             self.raise_user_error('No se ha definido un libro diario por defecto para %s', sale_device.name)
-            
+
         if statement :
             for s in statement:
                 tipo_p = s.tipo_pago
@@ -387,13 +464,13 @@ class WizardSalePayment(Wizard):
                 pass
             else:
                 self.raise_user_error('not_tipo_p')
-        else: 
+        else:
              self.raise_user_error('No ha creado un estado de cuenta para %s ', sale_device.name)
-             
-        
+
+
         if not sale.check_enough_stock():
             return
-        
+
         Product = Pool().get('product.product')
         if sale.lines:
             # get all products
@@ -410,7 +487,7 @@ class WizardSalePayment(Wizard):
                     products,
                     sale.get_enough_stock_qty(),
                     )
-            
+
             # check enough stock
             for line in sale.lines:
                 if line.product.type not in PRODUCT_TYPES:
@@ -421,38 +498,38 @@ class WizardSalePayment(Wizard):
                     if qty < line.quantity:
                         if not in_group():
                             self.raise_user_error('No hay suficiente stock del producto: \n %s \n en la bodega %s', (line.product.name, sale.warehouse.name))
-                
+
                         line.raise_user_warning('not_enough_stock_%s' % line.id,
                                'No hay suficiente stock del producto: "%s"'
                             'en la bodega "%s", para realizar esta venta.', (line.product.name, sale.warehouse.name))
                         # update quantities
                         quantities[line.product.id] = qty - line.quantity
-    
+
         if user.id != 0 and not sale_device:
             self.raise_user_error('not_sale_device')
-            
+
         term_lines = sale.payment_term.compute(sale.total_amount, sale.company.currency,
             sale.sale_date)
-            
+        total = sale.total_amount
         if not term_lines:
             term_lines = [(Date.today(), total)]
-        
+
         if sale.paid_amount:
-            payment_amount = sale.total_amount - sale.paid_amount  
-        else: 
+            payment_amount = sale.total_amount - sale.paid_amount
+        else:
             payment_amount = sale.total_amount
-            
+
         for date, amount in term_lines:
             if date == Date.today():
                 if amount < 0 :
-                    amount *=-1 
+                    amount *=-1
                 payment_amount = amount
-               
+
         if sale.paid_amount:
-            amount = sale.total_amount - sale.paid_amount  
-        else: 
+            amount = sale.total_amount - sale.paid_amount
+        else:
             amount = sale.total_amount
-        
+
         if payment_amount < amount:
             to_pay = payment_amount
         elif payment_amount > amount:
@@ -460,7 +537,7 @@ class WizardSalePayment(Wizard):
 
         else:
             to_pay= amount
-        
+
         return {
             'journal': sale_device.journal.id
                 if sale_device.journal else None,
@@ -469,7 +546,7 @@ class WizardSalePayment(Wizard):
             'currency_digits': sale.currency_digits,
             'party': sale.party.id,
             'tipo_p':tipo_p,
-            }     
+            }
 
     def transition_pay_(self):
         pool = Pool()
@@ -489,7 +566,9 @@ class WizardSalePayment(Wizard):
         sale = Sale(active_id)
         date = Pool().get('ir.date')
         date = date.today()
-        
+        if form.payment_amount == 0 and form.party.vat_number == '9999999999999':
+            self.raise_user_error('No se puede dar credito a consumidor final, monto a pagar no puede ser %s', form.payment_amount)
+
         if form.tipo_p == 'cheque':
             sale.tipo_p = form.tipo_p
             sale.banco = form.banco
@@ -499,7 +578,7 @@ class WizardSalePayment(Wizard):
             sale.numero_cheque = form.numero_cheque
             sale.sale_date = date
             sale.save()
-            
+
         if form.tipo_p == 'deposito':
             sale.tipo_p = form.tipo_p
             sale.banco_deposito = form.banco_deposito
@@ -508,7 +587,7 @@ class WizardSalePayment(Wizard):
             sale.numero_deposito= form.numero_deposito
             sale.sale_date = date
             sale.save()
-            
+
         if form.tipo_p == 'tarjeta':
             sale.tipo_p = form.tipo_p
             sale.numero_tarjeta = form.numero_tarjeta
@@ -516,14 +595,14 @@ class WizardSalePayment(Wizard):
             sale.tarjeta = form.tarjeta
             sale.sale_date = date
             sale.save()
-            
+
         if form.tipo_p == 'efectivo':
             sale.tipo_p = form.tipo_p
             sale.recibido = form.recibido
             sale.cambio = form.cambio_cliente
             sale.sale_date = date
             sale.save()
-            
+
         if not sale.reference:
             Sale.set_reference([sale])
 
@@ -531,7 +610,7 @@ class WizardSalePayment(Wizard):
             and sale.party.account_receivable.id
             or self.raise_user_error('party_without_account_receivable',
                 error_args=(sale.party.name,)))
-        
+
         if form.payment_amount:
             payment = StatementLine(
                 statement=statements[0].id,
@@ -543,22 +622,22 @@ class WizardSalePayment(Wizard):
                 sale=active_id
                 )
             payment.save()
-            
+
         if sale.acumulativo != True:
             sale.description = sale.reference
             sale.save()
             Sale.workflow_to_end([sale])
             Invoice = Pool().get('account.invoice')
             invoices = Invoice.search([('description', '=', sale.description)])
-            
+
             if sale.total_amount == sale.paid_amount:
                 return 'print_'
                 return 'end'
-                
+
             if sale.total_amount != sale.paid_amount:
                 return 'print_'
                 return 'end'
-                
+
             if sale.state != 'draft':
                 return 'print_'
                 return 'end'
@@ -571,12 +650,12 @@ class WizardSalePayment(Wizard):
             sale.save()
 
             Sale.workflow_to_end([sale])
-        
+
         return 'end'
-        
+
 class InvoiceReportPos(Report):
     __name__ = 'nodux_sale_payment.invoice_pos'
-    
+
     @classmethod
     def parse(cls, report, records, data, localcontext):
         pool = Pool()
@@ -605,18 +684,18 @@ class InvoiceReportPos(Report):
             for t in termlines:
                 t_f = t
                 cont += 1
-                
+
         if cont == 1 and t_f.days == 0:
             forma = 'CONTADO'
         else:
             forma = 'CREDITO'
-        
+
         if sale.total_amount:
             d = str(sale.total_amount)
             decimales = d[-2:]
         else:
             decimales='0.0'
-            
+
         user = User(Transaction().user)
         localcontext['user'] = user
         localcontext['company'] = user.company
@@ -632,28 +711,28 @@ class InvoiceReportPos(Report):
         localcontext['lineas'] = cls._get_lineas(Sale, sale)
         #localcontext['fecha_de_emision']=cls._get_fecha_de_emision(Invoice, invoice)
         return super(InvoiceReportPos, cls).parse(report, records, data,
-                localcontext=localcontext)   
-                
+                localcontext=localcontext)
+
     @classmethod
     def _get_amount_to_pay_words(cls, Sale, sale):
         amount_to_pay_words = Decimal(0.0)
         if sale.total_amount and conversor:
             amount_to_pay_words = sale.get_amount2words(sale.total_amount)
         return amount_to_pay_words
-        
+
     @classmethod
     def _get_lineas(cls, Sale, sale):
         cont = 0
-                
+
         for line in sale.lines:
             cont += 1
         return cont
-        
+
     @classmethod
     def _get_descuento(cls, Sale, sale):
         descuento = Decimal(0.00)
         descuento_parcial = Decimal(0.00)
-                
+
         for line in sale.lines:
             descuento_parcial = Decimal(line.product.template.list_price - line.unit_price)
             if descuento_parcial > 0:
@@ -661,24 +740,24 @@ class InvoiceReportPos(Report):
             else:
                 descuento = Decimal(0.00)
         return descuento
-    
+
     @classmethod
     def _get_subtotal_12(cls, Sale, sale):
         subtotal12 = Decimal(0.00)
         pool = Pool()
-        
+
         for line in sale.lines:
             if  line.taxes:
                 for t in line.taxes:
                     if str('{:.0f}'.format(t.rate*100)) == '12':
                         subtotal12= subtotal12 + (line.amount)
         return subtotal12
-                 
+
     @classmethod
     def _get_subtotal_0(cls, Sale, sale):
         subtotal0 = Decimal(0.00)
         pool = Pool()
-        
+
         for line in sale.lines:
             if  line.taxes:
                 for t in line.taxes:
@@ -700,9 +779,9 @@ class ReturnSale(Wizard):
     reverse_ = StateAction('sale.act_sale_form')
     def do_return_(self, action):
         Sale = Pool().get('sale.sale')
-        
+
         sales = Sale.browse(Transaction().context['active_ids'])
-        
+
         origin = str(sales)
         def in_group():
             pool = Pool()
@@ -732,7 +811,7 @@ class ReturnSale(Wizard):
         if len(return_sales) == 1:
             action['views'].reverse()
         return action, data
-        
+
     def do_reverse_(self, action):
         Sale = Pool().get('sale.sale')
         sales = Sale.browse(Transaction().context['active_ids'])
